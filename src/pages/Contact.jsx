0c6e60ed1a5ebeef99af5a786db527aa6f30e3e6
BrennanTanner <brenntanner@gmail.com>
2023-09-26 18:06:58 -0600

    another try to fix vercel

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,17 +9,6 @@ import '../styles/main.css';
 const theme = createTheme(dark);
 
 function Contact() {
-   const particlesInit = useCallback(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      await loadFull(engine);
-   }, []);
-
-   const particlesLoaded = useCallback(async (container) => {
-      //console.log(container);
-   }, []);
-
    return (
       <ThemeProvider theme={theme}>
          <CssBaseline />
